b5a5ba2d90fdb90b9bd39278f63e02b63d1f5b3f
willsmaness-bit <willsmaness@gmail.com>
2026-08-26 14:12:19 -0500

    Add vote board and draft prep for tomorrow night

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 (function () {
   const L = window.LEAGUE;
   const here = (location.pathname.split('/').pop() || 'index.html');
-  const links = [['index.html','Home'],['draft.html','Draft HQ'],['teams.html','Teams'],['standings.html','Standings'],['matchups.html','Matchups'],['rules.html','Rules'],['gazette.html','Gazette'],['records.html','Records']];
+  const links = [['index.html','Home'],['draft.html','Draft HQ'],['votes.html','Votes'],['teams.html','Teams'],['standings.html','Standings'],['matchups.html','Matchups'],['rules.html','Rules'],['gazette.html','Gazette'],['records.html','Records']];
   const header = document.getElementById('site-header');
   if (header) {
     header.innerHTML = '<div class="bar"><a class="brand" href="index.html"><span><span class="brand-full">'+L.shortName+'</span><span class="brand-short">NTFFL</span><span class="subbrand">'+L.sleeperName+'</span></span></a><button class="nav-toggle" type="button" aria-expanded="false">Menu</button><nav id="nav">'+links.map(([h,l])=>'<a href="'+h+'"'+(h===here?' aria-current="page"':'')+'>'+l+'</a>').join('')+'</nav></div>';
